585d7435b9053fac7135ad095d0dba9e1910794c
Vedanti M D <vedantideshmukh34@gmail.com>
2025-09-10 13:13:55 +0530

    Update macro.cpp

--- macro.cpp
+++ macro.cpp
@@ -8,16 +8,18 @@ int main()
     cout << "Max (100, 1000):";
     int k = MAXIMUM(100, 1000);
     cout << k << endl;
+   
     cout << "Max (20, 345):";
     int k1 = MAXIMUM(20, 0);
     cout << k1 << endl;
 
     cout << "Min (100, 1000):";
-
     int k2 = Minimum(100, 1000);
     cout << k2 << endl;
+   
     cout << "Min (20, 987):";
     int k3 = Minimum(20, 987);
     cout << k3;
     return 0;
+
 }
